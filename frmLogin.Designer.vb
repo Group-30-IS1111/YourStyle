8c4add4677a7c0e49703f39e21fc41ab098d544a
119376016 <61873813+119376016@users.noreply.github.com>
2020-04-07 15:23:54 +0100

    Add files via upload

---
+++ frmLogin.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnRegister = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LoginDataSet = New Diarmuid_s_Work.LoginDataSet()
+        Me.LoginTableAdapter = New Diarmuid_s_Work.LoginDataSetTableAdapters.LoginTableAdapter()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LoginDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnRegister
+        '
+        Me.btnRegister.Location = New System.Drawing.Point(176, 142)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Size = New System.Drawing.Size(75, 30)
+        Me.btnRegister.TabIndex = 12
+        Me.btnRegister.Text = "Register"
+        Me.btnRegister.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(91, 142)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 30)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(6, 142)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 30)
+        Me.btnLogin.TabIndex = 10
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(117, 59)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 22)
+        Me.txtPassword.TabIndex = 9
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(117, 6)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 22)
+        Me.txtUsername.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 17)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Password:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Username:"
+        '
+        'BindingSource1
+        '
+        Me.BindingSource1.DataMember = "Login"
+        Me.BindingSource1.DataSource = Me.LoginDataSet
+        '
+        'LoginDataSet
+        '
+        Me.LoginDataSet.DataSetName = "LoginDataSet"
+        Me.LoginDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'LoginTableAdapter
+        '
+        Me.LoginTableAdapter.ClearBeforeFill = True
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(264, 186)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmLogin"
+        Me.Text = "Login"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LoginDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRegister As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents LoginDataSet As LoginDataSet
+    Friend WithEvents LoginTableAdapter As LoginDataSetTableAdapters.LoginTableAdapter
+End Class
---
+++ frmLogin.vb
@@ -0,0 +1,50 @@
+﻿Public Class frmLogin
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LoginDataSet.Login' table. You can move, or remove it, as needed.
+        Me.LoginTableAdapter.Fill(Me.LoginDataSet.Login)
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtUsername.Clear()
+        txtPassword.Clear()
+    End Sub
+
+    Private Sub btnRegister_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
+        frmRegister.Show()
+        txtUsername.Clear()
+        txtPassword.Clear()
+        Me.Hide()
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        Dim queryUser = From Login In LoginDataSet.Login
+                        Where Login.Username = txtUsername.Text
+                        Select Login.Username
+
+        Dim queryPass = From Login In LoginDataSet.Login
+                        Where Login.Password = txtPassword.Text
+                        Select Login.Password
+
+        Dim FoDUsername = queryUser.FirstOrDefault()
+        Dim FoDPassword = queryPass.FirstOrDefault()
+
+        If txtUsername.Text = "" Then
+            MessageBox.Show("Please enter a username", "Error")
+            txtUsername.Focus()
+        ElseIf txtPassword.Text = "" Then
+            MessageBox.Show("Please enter a password", "Error")
+            txtPassword.Focus()
+        ElseIf FoDUsername.ToString() <> txtUsername.Text And
+           FoDPassword.ToString() <> txtPassword.Text Then
+            MessageBox.Show("Incorrect Username or Password", "Error")
+        ElseIf FoDUsername.ToString() = txtUsername.Text And
+           FoDPassword.ToString() = txtPassword.Text Then
+            MessageBox.Show("Login Successful")
+            Me.Hide()
+            'Next form needs to be put in here
+        End If
+
+    End Sub
+End Class
